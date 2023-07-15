57856078eba2130d0a56f4bda2d16aaf2d475527
Priyansh Mishra <priyanshmishra20052003@gmail.com>
2023-07-15 22:10:51 +0530

    new host configured

--- src/actions/host.js
+++ src/actions/host.js
@@ -1,3 +1,5 @@
-export const host = "https://priyansh-cb-backend.onrender.com";
+// export const host = "https://priyansh-cb-backend.onrender.com";
+
+export const host = "https://test-backend-ztu2.onrender.com";
 
 // export const host  = "http://localhost:5000"
